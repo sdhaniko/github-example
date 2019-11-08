89d53a71f6a8310fea904b90a5eb0bf6ba066fd3
sdhaniko <38313183+sdhaniko@users.noreply.github.com>
2019-11-08 06:44:57 +0530

    Capstone Task1

---
+++ cp_task1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # capstone project
+# ## Here I am starting my first project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
